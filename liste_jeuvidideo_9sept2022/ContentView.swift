f68b6396cc0921e5c56f727b18ff0038ccda13df
jsertram63 <jser>
2022-09-09 12:18:58 +0200

    Initial Commit

---
+++ liste_jeuvidideo_9sept2022/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  liste_jeuvidideo_9sept2022
+//
+//  Created by Lunack on 09/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            List(personnages){ perso in
+                PersonnageRow(perso: perso)
+            }
+            .navigationTitle("Personnage de jeu")
+            .listStyle(.plain)
+        }
+        
+    }
+}
+
+
+struct PersonnageRow: View {
+    let perso: Personnage
+    
+    var body: some View {
+        
+        HStack {
+            AsyncImage(url: URL(string: perso.imageName)!){ image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.black))
+                    
+                
+            }placeholder: {
+                ProgressView()
+            }
+            
+            
+            /*Image(systemName: "gamecontroller.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+                .cornerRadius(50)*/
+            VStack(alignment:.leading){
+                Text(perso.nomPersonnage)
+                    .font(.system(size: 21,
+                                  weight: .medium,
+                                  design: .default))
+                Text(perso.description)
+                Text(perso.nomDuJeuAppartenance)
+                    .font(.system(size: 14))
+            }
+        }
+       
+        
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ liste_jeuvidideo_9sept2022/liste_jeuvidideo_9sept2022App.swift
@@ -0,0 +1,17 @@
+//
+//  liste_jeuvidideo_9sept2022App.swift
+//  liste_jeuvidideo_9sept2022
+//
+//  Created by Lunack on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct liste_jeuvidideo_9sept2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
