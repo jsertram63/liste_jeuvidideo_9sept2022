3916cf972404a8f0857588a1be359f038fa5b0cb
PierricM380 <pierric.marye@wanadoo.fr>
2022-09-10 10:15:00 +0200

    Amélioration du design

--- liste_jeuvidideo_9sept2022/View/ContentView.swift
+++ liste_jeuvidideo_9sept2022/View/ContentView.swift
@@ -11,48 +11,71 @@ struct ContentView: View {
     @EnvironmentObject var jeuVideoVM: JeuVideoViewModel
     
     var body: some View {
-        NavigationView{
-            List(jeuVideoVM.personnages, id: \.id) { perso in
-                NavigationLink {
-                    // DetailView( : )
-                } label: {
-                    HStack {
-                        AsyncImage(url: URL(string: perso.imageName)!) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                                .overlay(Circle().stroke(.black))
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        
-                        VStack(alignment:.leading){
-                            Text(perso.nomPersonnage)
-                                .font(.system(size: 21,
-                                              weight: .medium,
-                                              design: .default))
-                            Text(perso.nomDuJeuAppartenance)
-                                .font(.system(size: 14))
-                        }
-                    }
-                    .padding(.vertical)
-                }
-                .listRowBackground(Color("Color2"))
-            }
-            .background(Color("Color2").ignoresSafeArea())
-            .navigationTitle("Héros de jeux vidéo")
-            .onAppear {
-                UINavigationBar.appearance().largeTitleTextAttributes = [
-                    .foregroundColor: UIColor.tintColor,
-                    .font : UIFont(name:"Noteworthy", size: 30)!
-                ]
+        NavigationView {
+            ZStack {
+                Color("Color1").ignoresSafeArea()
                 
-                UITableView.appearance().backgroundColor = .clear
+                VStack {
+                    Divider()
+                        .background(Color("Color2"))
+                    
+                    List(jeuVideoVM.personnages, id: \.id) { perso in
+                        NavigationLink {
+                            // DetailView( : )
+                        } label: {
+                            HStack(spacing: 15.0) {
+                                AsyncImage(url: URL(string: perso.imageName)!) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 50, height: 50)
+                                        .clipShape(Circle())
+                                        .overlay(
+                                            Circle()
+                                                .stroke(.tint, lineWidth: 2)
+                                                .padding(-5.0)
+                                                .shadow(radius: 5)
+                                        )
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                
+                                VStack(alignment:.leading){
+                                    Text(perso.nomPersonnage)
+                                        .font(.system(size: 21,
+                                                      weight: .medium,
+                                                      design: .default))
+                                    Text(perso.nomDuJeuAppartenance)
+                                        .font(.system(size: 14))
+                                }
+                            }
+                            .padding(.vertical, 8.0)
+                        }
+                        .listRowBackground(Color("Color2"))
+                    }
+                    .navigationBarTitle("Héros de jeux vidéo")
+                    .navigationBarItems(
+                        trailing:
+                            Image("GameOne")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(.tint)
+                            )
+                    )
+                    .onAppear {
+                        UINavigationBar.appearance().largeTitleTextAttributes = [
+                            .foregroundColor: UIColor.tintColor
+                        ]
+                        
+                        UITableView.appearance().backgroundColor = .clear
+                    }
+                }
             }
         }
-        
     }
 }
 
--- liste_jeuvidideo_9sept2022/model/PersonnageModel.swift
+++ liste_jeuvidideo_9sept2022/model/PersonnageModel.swift
@@ -7,25 +7,11 @@
 
 import Foundation
 
-
-struct PersonnageModel: Identifiable { // protocol Identifiable pour implémenter id
-    
+// protocol Identifiable pour implémenter id
+struct PersonnageModel: Identifiable {
     let id = UUID()
-    let nomPersonnage:String
+    let nomPersonnage: String
     let nomDuJeuAppartenance: String
     let description: String
-    let imageName:String
-    
+    let imageName: String
 }
-
-
-let personnages = [
-    PersonnageModel(nomPersonnage: "Mario",
-               nomDuJeuAppartenance: "Mario",
-               description: "Plombier",
-               imageName: "https://cdn-icons-png.flaticon.com/512/528/528107.png"),
-   PersonnageModel(nomPersonnage: "Batman", nomDuJeuAppartenance: "Bruce wayne", description: "la Chauve Souris", imageName:"https://cdn-icons-png.flaticon.com/512/805/805385.png"),
-   PersonnageModel(nomPersonnage: "Sonic", nomDuJeuAppartenance: "Sonic Herzog", description: "Hérisson", imageName: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0e738c17-7f3c-422e-8225-f8c782b08626/d8obg8n-b0b4af2d-8b88-4250-9a33-325a4616ca38.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBlNzM4YzE3LTdmM2MtNDIyZS04MjI1LWY4Yzc4MmIwODYyNlwvZDhvYmc4bi1iMGI0YWYyZC04Yjg4LTQyNTAtOWEzMy0zMjVhNDYxNmNhMzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.sB3LsRfQcumMxBQ0xn81xfm235MmWU0WTOJm_3bZ1No"),
-  PersonnageModel(nomPersonnage: "Yoshi", nomDuJeuAppartenance: "Mario Kart", description: "dinosaure", imageName: "https://cdn-icons-png.flaticon.com/512/7518/7518463.png")
-
-]
